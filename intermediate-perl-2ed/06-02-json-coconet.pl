99e2c0648537668a1d9b95dce55ffd69bebb0a1b
Matthew L Davis <mld.0@protonmail.com>
2021-06-27 19:39:15 +1000

    ongoing - intermediate-perl-2ed exercises
    indent block contents

--- intermediate-perl-2ed/06-02-json-coconet.pl
+++ intermediate-perl-2ed/06-02-json-coconet.pl
@@ -23,20 +23,20 @@ close FH or die "Couldn't close input_path=($input_path): $!\n";
 #	Write %total_bytes to $jsonfile_path as JSON
 my $jsonfile_path = "total_bytes.json";
 {
-open FH_JSON, '>:utf8', $jsonfile_path or die "Failed to open for writing jsonfile_path=($jsonfile_path): $!\n";
-print FH_JSON to_json(\%total_bytes, { pretty => 1 });
-close FH_JSON or die "Failed to close jsonfile_path=($jsonfile_path): $!\n";
+	open FH_JSON, '>:utf8', $jsonfile_path or die "Failed to open for writing jsonfile_path=($jsonfile_path): $!\n";
+	print FH_JSON to_json(\%total_bytes, { pretty => 1 });
+	close FH_JSON or die "Failed to close jsonfile_path=($jsonfile_path): $!\n";
 }
 
 #	Read JSON from jsonfile_path to %total_bytes
 %total_bytes = ();
 -e "$jsonfile_path" or die "File not found jsonfile_path=($jsonfile_path): $!\n";
 {
-local $/ = undef;  # slurp mode
-open FH_JSON, '<:raw', $jsonfile_path or die "Failed to open for reading jsonfile_path=($jsonfile_path): $!\n";
-my $json_str = <FH_JSON>;  # read entire file as string
-%total_bytes = %{decode_json($json_str)};  # dereference decode_json() result
-close FH_JSON or die "Failed to close jsonfile_path=($jsonfile_path): $!\n";
+	local $/ = undef;  # slurp mode
+	open FH_JSON, '<:raw', $jsonfile_path or die "Failed to open for reading jsonfile_path=($jsonfile_path): $!\n";
+	my $json_str = <FH_JSON>;  # read entire file as string
+	%total_bytes = %{decode_json($json_str)};  # dereference decode_json() result
+	close FH_JSON or die "Failed to close jsonfile_path=($jsonfile_path): $!\n";
 }
 
 print Dumper(%total_bytes);
