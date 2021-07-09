da16b3ee660d4830b53e44a0cf985fb94d9d25e4
Matthew L Davis <mld.0@protonmail.com>
2021-07-09 15:12:12 +1000

    ongoing - perl-one-liners exercises

--- perl-oneliners/06-modules-shuffle-xml-datetime-exercises.sh
+++ perl-oneliners/06-modules-shuffle-xml-datetime-exercises.sh
@@ -14,7 +14,14 @@ echo "$s" | perl -MList::Util=shuffle -anE 'say( join " ", (map { join "", shuff
 echo ""
 
 echo "c)"
-#cat "sample.xml" | perl -nE ''
+cat "sample.xml" | perl -MXML::LibXML -E '$ip = XML::LibXML->load_xml(IO => STDIN); say $_->to_literal() for $ip->findnodes("//blue")'
+#	or
+#perl -MXML::LibXML -E '$ip = XML::LibXML->load_xml(location => $ARGV[0]); say $_->to_literal() for $ip->findnodes("//blue")' "sample.xml"
 echo ""
 
+#	Current time in given format
+echo "d)"
+f='%FT%H:%M:%S%Z'
+perl -MDateTime -sE 'say DateTime->now(time_zone=>q/local/)->strftime($f)' -- -f="$f"
+echo ""
 
