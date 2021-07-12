ac431c0deffab1209147f74e7f01497fa30b046f
Matthew L Davis <mld.0@protonmail.com>
2021-07-12 18:27:27 +1000

    learn-more-python-hard-way exercises, sed
    'Partial' implementation due to no apparent (straightforward?) support
    for BRE and ERE in python - this implementation breaks up
    s/find/replace/options and calls re.sub() with these values.
    'options' not yet implemented, leaving most sed options still
    unimplemented. Rudimentary tests comapre script vs sed with basic
    substitution on both pipe and file.

--- learn-more-python-hard-way/09-sed/implement-sed.py
+++ learn-more-python-hard-way/09-sed/implement-sed.py
@@ -5,89 +5,121 @@
 import sys
 import os
 import argparse
+import re
 import logging
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 #   {{{2
 self_version = "0.0"
 
-def _script_input(arg_script):
-    #   Ongoing: 2021-07-11T21:57:55AEST when is something considered a script by sed? Quoted/unquoted string? If it matches a filename/path? <- not considered a file unless accompanyed by '-f'? 
-    return arg_script
+#def _script_input(arg_script):
+#    return arg_script
 
 def _in_place_input(arg_suffix):
     return arg_suffix
 
 def _open_if_valid_file(arg):
-    """Return an open filehandle to a valid file, otherwise return arg as string with a warning"""
+    """Return an open filehandle to a valid file, otherwise return arg as string with an error"""
     #   {{{
+    if os.path.isdir(arg):
+        logging.error("non-file arg=(%r)" % arg)
+        sys.stderr.write("Can't read %s: Is a directory\n" % arg)
+        exit(4)
     if not os.path.isfile(arg):
-        logging.warning("skip non-file arg=(%r)" % arg)
-        return arg
+        logging.error("non-file arg=(%r)" % arg)
+        sys.stderr.write("Can't read %s: No such file or directory\n" % arg)
+        exit(2)
     else:
         return open(arg, 'r')  
     #   }}}
-    
-parser = argparse.ArgumentParser(prog='python-sed')
 
+def _seperate_and_validate_cmd(arg_cmd):
+    """Validate and split s(sep)find(sep)replace(sep)options, returning the tuple (sep, match, replace, options)"""
+    #   {{{
+    logging.debug("arg_cmd=(%r)" % arg_cmd)
+    if type(arg_cmd) is list:
+        if len(arg_cmd) > 1:
+            logging.error("(Should not happen) len sublist arg_cmd > 1, arg_cmd=(%r)" % arg_cmd)
+            exit(2)
+        arg_cmd = arg_cmd[0]
+    if not type(arg_cmd) is str:
+        logging.error("Invalid arg_cmd: Must be str, type(arg_cmd)=(%r)" % type(arg_cmd))
+        exit(2)
+    if not re.match("^s", arg_cmd):
+        logging.error("Invalid arg_cmd: must begin with 's'")
+        exit(2)
+    if len(arg_cmd) < 4:
+        logging.error("Invalid arg_cmd: must be at least 4 characters")
+        exit(2)
+    #   arg_cmd must be of the form s(sep)match(sep)replace(sep)options, 
+    candidate_sep = arg_cmd[1]
+    #search_sep = re.findall(candidate_sep, arg_cmd)
+    search_sep = [letter for i, letter in enumerate(arg_cmd) if letter == candidate_sep]
+    if len(search_sep) < 3:
+        logging.error("Invalid arg_cmd: Must contain at least 3 candidate_sep=(%r), arg_cmd=(%r)" % (candidate_sep, arg_cmd))
+        exit(2)
+    if len(search_sep) > 3:
+        logging.error("Invalid arg_cmd: Must contain no more than 3 candidate_sep=(%r), arg_cmd=(%r)" % (candidate_sep, arg_cmd))
+        exit(2)
+    sep = candidate_sep
+    arg_cmd_split = arg_cmd.split(sep)
+    match = arg_cmd_split[1]
+    replace = arg_cmd_split[2]
+    options = arg_cmd_split[3]
+    logging.debug("sep=(%r), match=(%r), replace=(%r), options=(%r)" % (sep, match, replace, options))
+    return ( sep, match, replace, options )
+    #   }}}
+
+parser = argparse.ArgumentParser(prog='python-sed')
+#   {{{
 parser.add_argument('--version', action='version', version='%(prog)s ' + str(self_version))
 parser.add_argument('-n', '--quiet', '--silent', action='store_true', help="")
 
-parser.add_argument('-e', '--expression', nargs='+', help="")
-parser.add_argument('-E', '-r', '--regexp-extended', nargs='+', help="")
-parser.add_argument('--pcre', nargs='+', help="Use perl regex")
-parser.add_argument('--pyre', nargs='+', help="Use python regex")
+parser.add_argument('-e', '--expression', action='append', nargs=1, help="")
 
-parser.add_argument('cmd_str', action='store', type=_script_input, nargs=1, help="")
+#   Ongoing: 2021-07-12T18:07:07AEST learn-more-python-hard-way, 09/implement-sedp.y, This is complicated substantially, by python not having support for BRE or ERE? 
+#parser.add_argument('-E', '-r', '--regexp-extended', action='store_true', help="")  # Not applicable, ERE not supported
+#parser.add_argument('--pcre', action='store_true', help="Use perl regex")
+#parser.add_argument('--pyre', action='store_true', help="Use python regex")
 
-parser.add_argument('-f', '--file', nargs=1, help="")
+#parser.add_argument('cmd_str', action='store', type=_script_input, nargs='?', help="")
+parser.add_argument('cmd_str', action='store', nargs='?', help="")
 
-parser.add_argument('-s', '--seperate', help="")
-parser.add_argument('-z', '--null-data', help="")
-parser.add_argument('-i', '--in-place', type=_in_place_input, nargs='?', help="")
+parser.add_argument('-f', '--file', default=[], action='append', nargs=1, help="")
 
-parser.add_argument('--follow-symlinks', action='store_true', help="")
+#parser.add_argument('-s', '--seperate', help="")
+#parser.add_argument('-z', '--null-data', help="")
+#parser.add_argument('-i', '--in-place', type=_in_place_input, nargs='?', help="")
+#parser.add_argument('--follow-symlinks', action='store_true', help="")
 
-parser.add_argument('files', nargs='*', default=sys.stdin, type=_open_if_valid_file, help="Specify file(s), (or default to stdin)")
+parser.add_argument('input', nargs='*', default=sys.stdin, type=_open_if_valid_file, help="Specify file(s), (or default to stdin)")
+#   }}}
 
 args = parser.parse_args()
 logging.debug("args=(%r)" % args)
+if not type(args.input) is list:
+    args.input = [ args.input ]
+if (args.expression):  # If given one-or-more regex strings using '-e', cmd_str (the first positional argument) is treated as a file, not used as regex string
+    if (args.cmd_str):
+        args.input += [ _open_if_valid_file(args.cmd_str) ]
+else: 
+    if (args.cmd_str):
+        #cmds_sed += [ args.cmd_str ]
+        args.expression = [ args.cmd_str ] 
+logging.debug("args=(%r)" % args)
 
+if (args.file):
+    logging.debug("file=(%r) not supported" % args.file)
+    exit(2)
 
-#   Note: 
-#       sed -E -f "file" 
-#   is a valid sed command, as is 
-#       sed -E "regex"
-#   however the following is not:
-#       sed -e -f "file"
-
-#   sed (gnu version?)
-#       sed [OPTION]... {script-only-if-no-other-script} [input-file]...
-#   -h, --help
-#   --version
-#   --debug
-#   -n, --quiet, --silent               supress (automatic) printing of pattern space
-#   
-#   -i[SUFFIX], --in-place[=SUFFIX]     edit files in place (backup file using SUFFIX if given)
-#
-#   -l N, --line-length=N
-#   -s, --seperate                      consider files as seperate rather than a single 
-#   
-#   --sandbox                           disable e/r/w
-#   -z, --null-data             Seperate lines by NUL characters
-#
-#   -f script-file, --file=script-file
-#
-#   Regex versions:
-#       BRE         -e script, --expression=script
-#       ERE         (-E,-r) script, --regexp-extended=script
-#       pcre        <--pcre>                                      
-#       python      <--pyre>
-#   
-#   yeah nah options:
-#   {{{
-#   --follow-symlinks
-#   -u, --unbuffered
-#   }}}
+for loop_file in args.input:
+    for loop_line in loop_file:
+        logging.debug("loop_line=(%r)" % loop_line)
+        for loop_cmd_str in args.expression:
+            result = _seperate_and_validate_cmd(loop_cmd_str)
+            (loop_sep, loop_match, loop_replace, loop_options) = result
+            loop_line = re.sub(loop_match, loop_replace, loop_line)  # flags? 
+            logging.debug("loop_line=(%r)" % loop_line)
+        print(loop_line, end='')
 
 
 #   Sed Help:
--- learn-more-python-hard-way/09-sed/test.py
+++ learn-more-python-hard-way/09-sed/test.py
@@ -1,3 +1,7 @@
+#   VIM SETTINGS: {{{3
+#   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+#   vim: set foldlevel=2 foldcolumn=3:
+#   }}}1
 import sys
 import os
 import argparse
@@ -6,20 +10,24 @@ import logging
 import subprocess
 import unittest
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-
+#   {{{2
 test_bin_python = "/usr/local/bin/python3"
 
 test_script_sed = "implement-sed.py"
+#   {{{
 if not (os.path.isfile(test_script_sed)):
     self_dir = os.path.dirname(os.path.realpath(__file__))
     test_script_sed = os.path.join(self_dir, test_script_sed)
 assert os.path.isfile(test_script_sed), "Failed to find 'implement-find.py' and '%s'" % test_script_sed
+#   }}}
 
 test_data_dir = "../data_test"
+#   {{{
 if not (os.path.isdir(test_data_dir)):
     test_data_dir = "data_test"
 assert os.path.isdir(test_data_dir), "Failed to find '../data_test' and 'data_test'"
-    
+#   }}}
+
 test_file_path_csv = os.path.join(test_data_dir, "values.csv")
 test_file_path_allbytes = os.path.join(test_data_dir, "allbytes.hex")
 
@@ -27,30 +35,114 @@ class Test_ImplementSed(unittest.TestCase):
 
     #   TODO: 2021-07-11T22:17:29AEST Before tests, test for existance each filesystem item that needs to exists, then script version, and exit if either fail
 
-    def get_BinPython_Sed_OutputAndRC(self, args_binpython, args_sed, decode_result=True):
+    def get_BinPython_Pipe_Sed_OutputAndRC(self, args_binpython, args_sed, arg_input_pipe='', decode_result=True):
         args_binpython = [ test_bin_python, test_script_sed ] + args_binpython
-        proc = subprocess.Popen(args_binpython, stdout=subprocess.PIPE)
-        (out_test, err_test) = proc.communicate()
+        proc = subprocess.Popen(args_binpython, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        (out_test, err_test) = proc.communicate(input=arg_input_pipe.encode())
         rc_test = proc.returncode
         args_sed = [ 'sed' ] + args_sed
-        proc = subprocess.Popen(args_sed, stdout=subprocess.PIPE)
-        (out_check, err_check) = proc.communicate()
+        proc = subprocess.Popen(args_sed, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        (out_check, err_check) = proc.communicate(input=arg_input_pipe.encode())
         rc_check = proc.returncode
         if (decode_result):
             return (out_test.decode(), rc_test, out_check.decode(), rc_check)
         else:
             return (out_test, rc_test, out_check, rc_check)
 
+    #   Ongoing: 2021-07-12T17:41:47AEST Both fail due to unimplemented -E option
+    #   {{{
+    #def test_replace_pipe_aA_usingEee(self):
+    #    val_input = "abc"
+    #    val_expected = "Abc"
+    #    args = [ '-E', '-e', 's/a/A/', '-e', 's/A/a/' ]
+    #    result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+    #    (out_test, rc_test, out_check, rc_check) = result
+    #    self.assertEqual(out_test, out_check)
+    #    self.assertEqual(rc_test, rc_check)
+    #def test_replace_pipe_aA_usingE(self):
+    #    val_input = "abc"
+    #    val_expected = "Abc"
+    #    args = [ '-E', 's/a/A/' ]
+    #    result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+    #    (out_test, rc_test, out_check, rc_check) = result
+    #    self.assertEqual(out_test, out_check)
+    #    self.assertEqual(rc_test, rc_check)
+    #   }}}
+
+    def test_replace_pipe_capture(self):
+        val_input = "abc"
+        args = [ 's/(a)(bc)/\\2\\1/' ]
+        #   Need to enable ERE to use () capture groups
+        args_sed = [ '-E', 's/(a)(bc)/\\2\\1/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args_sed, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+
+    #   (Seemingly) working:
+    def test_replace_pipe_aAa_usingee(self):
+        val_input = "abc"
+        val_expected = "abc"
+        args = [ '-e', 's/a/A/', '-e', 's/A/a/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+    def test_replace_pipe_aAa_usingee_alt_seperators(self):
+        val_input = "abc"
+        val_expected = "abc"
+        args = [ '-e', 's.a.A.', '-e', 's|A|a|' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+    def test_replace_file_csv(self):
+        args = [ 's/1/0/g', test_file_path_csv ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, '')
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+    def test_replace_pipe_aA(self):
+        val_input = "abc"
+        val_expected = "Abc"
+        args = [ 's/a/A/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+    def test_replace_pipe_aA_usinge(self):
+        val_input = "abc"
+        val_expected = "Abc"
+        args = [ '-e', 's/a/A/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
     #   Others:
+    def test_invalid_args_cmdstr_followedby_e(self):
+        val_input = "abc"
+        args = [ 's/a/A/', '-e', 's/A/a/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
+    def test_invalid_args_cmdstrIsDir_followedby_e(self):
+        val_input = "abc"
+        args = [ test_data_dir, '-e', 's/A/a/' ]
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args, val_input)
+        (out_test, rc_test, out_check, rc_check) = result
+        self.assertEqual(out_test, out_check)
+        self.assertEqual(rc_test, rc_check)
     def test_getversion(self):
         """Verify version string starts with 'python-cut' followed by a decimal number"""
         args = [ '--version' ]
-        result = self.get_BinPython_Sed_OutputAndRC(args, args)
+        result = self.get_BinPython_Pipe_Sed_OutputAndRC(args, args)
         (out_test, rc_test, out_check, rc_check) = result
-        self.assertEqual(rc_test, rc_check)
         self.assertRegex(out_test, re.compile("^python-sed \d+\.\d+.*"))
+        self.assertEqual(rc_test, rc_check)
     #   TODO: incorrect args tests
-    
-    
+
+
 if __name__ == '__main__':
     unittest.main()
+
